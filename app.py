311035fb3e42187dae5443eb744addef9d0bbd7c
David Mullins <mullinsdavid88@gmail.com>
2023-03-23 09:29:22 -0400

    added our DB model

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from models import Base, session, Book, engine
+
+
 # import models file
 # main menu - add, search, analysis, exit, view
 # add books to the database
@@ -6,3 +9,7 @@
 # search books
 # data cleaning
 # loop - run program
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)

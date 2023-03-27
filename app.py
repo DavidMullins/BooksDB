eece0ea3982082d72d707718252d56eb96132a68
David Mullins <mullinsdavid88@gmail.com>
2023-03-27 10:10:10 -0400

    update add_csv() to add books to .db / remove comments

--- app.py
+++ app.py
@@ -32,19 +32,30 @@ def menu():
 def clean_date(date_str):
     months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     split_date = date_str.split(' ')
-    print(split_date)
     month = int(months.index(split_date[0]) + 1)
     day = int(split_date[1].split(',')[0])
-    print(day)
     year = int(split_date[2])
     return datetime.date(year, month, day)
+
+
+def clean_price(price_str):
+    price_float = float(price_str)
+    return int(price_float * 100)
 
 
 def add_csv():
     with open('suggested_books.csv') as csvfile:
         data = csv.reader(csvfile)
         for row in data:
-            print(row)
+            book_in_db = session.query(Book).filter(Book.title==row[0]).one_or_none()
+            if book_in_db == None:
+                title = row[0]
+                author = row[1]
+                date = clean_date(row[2])
+                price = clean_price(row[3])
+                new_book = Book(title=title, author=author, published_date=date, price=price)
+                session.add(new_book)
+        session.commit()
 
 
 def app():
@@ -72,5 +83,7 @@ def app():
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
     # app()
-    # add_csv()
-    clean_date('October 25, 2017')
+    add_csv()
+
+    for book in session.query(Book):
+        print(book)
